a9c671fec3789588f493a1f4ebd25c890fa97438
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-23 11:00:59 -0700

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_user_name(params[:user_name])
+    if user &&
+      user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      @sign_in_error = "Username / password combination is invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
