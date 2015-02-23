f7669e1e33452ca58adfeeb87b5e2f1acdffda23
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-23 12:36:51 -0700

    Add ability for users to create and update usersr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: 'User was successfully created'
+    else
+      flash.now[:alert] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
   def edit
     @user = User.find(params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
- validates :user_name, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
 
  has_secure_password
 end
