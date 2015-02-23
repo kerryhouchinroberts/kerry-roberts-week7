829e9501f061f62891b1fd87f09459f5feb9f380
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-23 14:13:38 -0700

    Remove rogue 's' that somehow snuck into 'resgistrations#create' in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
    resources :courses
 
    get '/sign-up' => 'registrations#new', as: :signup
-   post '/sign-up' => 'resgistrations#create'
+   post '/sign-up' => 'registrations#create'
    get '/sign-in' => 'sessions#new', as: :signin
    post '/sign-in' => 'sessions#create'
    get '/sign-out' => 'sessions#destroy', as: :signout
