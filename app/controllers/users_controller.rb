fe4b0f79299274e0eb29c9283c5a5d8738674286
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-23 11:23:58 -0700

    Create Users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,28 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to users_path, notice: 'User was successfully updated'
+    else
+      flash.now[:alert] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password, :about)
+  end
+
 end
